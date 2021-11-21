8b24f3c28c474de901ea88e637336caad5f6d2cb
Zhu Rengen <zhurungen@wecash.net>
2021-11-21 21:09:06 +0800

    update dubbo

--- dubbo/dubbo.go
+++ dubbo/dubbo.go
@@ -5,21 +5,22 @@ import (
 	"dubbo.apache.org/dubbo-go/v3/config"
 	_ "dubbo.apache.org/dubbo-go/v3/imports"
 	_ "github.com/dubbogo/triple/pkg/triple"
+	cartApi "github.com/fastmall/cart/api"
 	"github.com/fastmall/customer/service"
 	goodsApi "github.com/fastmall/goods/api"
+	orderApi "github.com/fastmall/order/api"
 )
 
 var GoodsService = &goodsApi.GoodsServiceClientImpl{}
+var CartService = &cartApi.CartServiceClientImpl{}
+var OrderService = &orderApi.OrderServiceClientImpl{}
 
 func StartDubbo() {
 	config.SetConsumerService(GoodsService)
+	config.SetConsumerService(CartService)
+	config.SetConsumerService(OrderService)
 	config.SetProviderService(&service.CustomerService{})
 	err := config.Load(config.WithPath("conf/dubbo.yaml"))
-	if err != nil {
-		logger.Fatal(err)
-		return
-	}
-	err = config.Load(config.WithPath("conf/dubbo.yaml"))
 	if err != nil {
 		logger.Fatal(err)
 		return
